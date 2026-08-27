b0f4b2a279a4f707851ad2e6a4eae07c2b81f618
Eagle Wu <62476170+mageeagle@users.noreply.github.com>
2026-08-27 18:19:43 +0900

    Fix AmbiPlayer initialization unclickable

--- components/sound/AmbiPlayerDemo.tsx
+++ components/sound/AmbiPlayerDemo.tsx
@@ -1,10 +1,14 @@
 import { useSection } from "@/hooks/useSection";
 import AmbiPlayer from "./AmbiPlayer";
+import { useEffect } from "react";
 
 export default function AmbiPlayerDemo() {
   const sectionOn = useSection((s) => s.sectionOn);
   const ind = useSection((s) => s.section);
   const main = useSection((s) => s.main);
+  useEffect(() => {
+      useSection.getState().setMapView(false);
+  }, [])
   return (
     <>
       {sectionOn && (main === "rift") && (ind !== 0) && (
